baefa4939b55261e5975c1a5508ec14d35312499
AshenEditZ <officialcyber97@outlook.com>
2025-12-30 19:21:07 +0530

    Refactor config.js to simplify CORS handling

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,26 +1,14 @@
 exports.handler = async function(event, context) {
-    if (event.httpMethod === 'OPTIONS') {
-        return {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods': 'GET, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type'
-            },
-            body: ''
-        };
-    }
-    
     return {
         statusCode: 200,
         headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
         },
         body: JSON.stringify({
-            HF_API_KEY: process.env.HF_API_KEY || '',
-            SUPABASE_URL: process.env.SUPABASE_URL || '',
-            SUPABASE_KEY: process.env.SUPABASE_KEY || ''
+            HF_API_KEY: process.env.HF_API_KEY || "",
+            SUPABASE_URL: process.env.SUPABASE_URL || "",
+            SUPABASE_KEY: process.env.SUPABASE_KEY || ""
         })
     };
 };
